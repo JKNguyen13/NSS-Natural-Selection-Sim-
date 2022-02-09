eb2bb8d938eda128136e5acc014a58aadbf3d0e6
J <jnguyen2737@student.pps.net>
2022-02-09 11:11:16 -0800

    Update (2/09)

--- Natural Selection Simulation/Assets/Player.cs
+++ Natural Selection Simulation/Assets/Player.cs
@@ -17,10 +17,14 @@ public class Player : MonoBehaviour
     //public GlobalData stam = new GlobalData();
 
     int time = 0;
+    int inc = 1;
     int final = 0;
-    int stamina = 8000;
+    
     float randomValue;
-
+    [SerializeField]
+    int stamina = 8000;
+    bool dontDie = true;
+    bool stop = true;
     [SerializeField]
     private bool swapNew = true;
     [SerializeField]
@@ -37,8 +41,8 @@ public class Player : MonoBehaviour
     }
 
     void Update()
-    {   //Debug.Log(GlobalData.spawnamount);
-        Debug.Log(stamina); 
+    {   
+        //Debug.Log(stamina); 
         time++;
         final++;
         amountnum++;
@@ -47,29 +51,39 @@ public class Player : MonoBehaviour
         if (amountnum < agentamount){
             Instantiate(agentclone,placementPosition(),Quaternion.identity);
         }
-
+        if(time == 400){
+            //inc = 0;
+            dontDie = true;
+        }
         if(time >= 400 && this.swapNew)
         {
             this.agent.destination = new Vector3(Random.Range(-45,45),0,Random.Range(-45,45));
             time = 0;
         }
 
-        if(stats.food >= 1){
+        if(stats.food >= 1 && stop){ //add && stamina < 2000
             this.swapNew = false;
             this.clear = false;
+            inc = 0;
             this.agent.destination = placementPosition();
             if (stats.food >= 2)
             {
                 GlobalData.spawnamount++;
             }
             stats.food = 0;
+            stop = false;
         }
+        
         if(stamina <= 0 && clear)
         {
             this.agent.destination = this.agent.transform.position;
         }
-        if (final >= 10000 && clear) {
-            Debug.Log(1);
+        if(this.agent.transform.position.x == 48 || this.agent.transform.position.x == -48 || this.agent.transform.position.z == 48 || this.agent.transform.position.z == -48){
+            Debug.Log("yay");
+            dontDie = false;
+        }
+        if (final >= 10000 && dontDie) {
+            //Debug.Log(1);
             GlobalData.amountOfAgents -= 1;
             GlobalData.averageSpeed -= agent.speed;
             Destroy(this.gameObject);
@@ -78,6 +92,9 @@ public class Player : MonoBehaviour
                 this.swapNew = true;
                 Debug.Log(2);
                 this.clear = true;
+                this.stop = true;
+                this.dontDie = true;
+                
             for(int i = 0; i < GlobalData.spawnamount; i++){
                 randomValue = Random.Range(-0.5f,3);
                 Transform instantiated = Instantiate(agentclone,placementPosition(),Quaternion.identity);
@@ -90,6 +107,8 @@ public class Player : MonoBehaviour
                 GlobalData.spawnamount = 0;
                 stamina = 8000;
                 final = 0;
+                stats.food = 0;
+                inc = 1;
     }
     
     
@@ -99,13 +118,13 @@ public class Player : MonoBehaviour
     Vector3 placementPosition(){
         int posi = Random.Range(1,4);
         if(posi == 1){
-            return new Vector3(Random.Range(-48,48),1 + randomValue,-48);
+            return new Vector3(Random.Range(-48 - inc,48 - inc),1 + randomValue,-48 - inc);
         }else if(posi == 2){
-            return new Vector3(Random.Range(-48,48),1 + randomValue,48);
+            return new Vector3(Random.Range(-48 - inc,48 - inc),1 + randomValue,48 - inc);
         }else if(posi == 3){
-            return new Vector3(-48,1 + randomValue,Random.Range(-48,48));
+            return new Vector3(-48 - inc,1 + randomValue,Random.Range(-48 - inc,48 - inc));
         }else if(posi == 4){
-            return new Vector3(48,1 + randomValue,Random.Range(-48,48));
+            return new Vector3(48 - inc,1 + randomValue,Random.Range(-48 - inc,48 - inc));
         }else{
             return new Vector3(0,1,0);
         }
